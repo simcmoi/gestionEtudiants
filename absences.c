6a5403e52b0c4dfe099f0741558def75de2df07c
simcmoi <simonfessy@gmail.com>
2017-03-15 14:06:48 +0100

    Update absences.c

--- absences.c
+++ absences.c
@@ -8,7 +8,7 @@
 #include "absences.h"
 
   printf("=====Absences=====!\n");
-        printf("21) Faire l'appel des étudiants dans une matière\n");
+        printf("21) Faire l'appel des Ã©tudiants dans une matiÃ¨re\n");
         printf("22) Modifier une abscence\n");
         printf("23) Supprimer une abscence\n");
         printf("24) Afficher les abscences\n\n");
@@ -29,4 +29,8 @@ void FaireAppel(PileAbsences *p, char *matiere)
     p->premier = newAbs;
     p->taille++;
 }
+void ModifierAbsences(PileAbsences *p, char *matiere){
+} 
 
+Void 
+  
