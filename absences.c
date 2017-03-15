03332dd21b4f63a3bf6da67e7b31f7c5cc6056a9
alexandre-szymkiw <alexandre.szymkiw@gmail.com>
2017-03-15 14:00:48 +0100

    init2

---
+++ absences.c
@@ -0,0 +1,32 @@
+/**Auteurs  : Prenom           Nom        **/
+/**           Alexandre        Szymkiw    **/
+/**           Simon            Fessy      **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "absences.h"
+
+  printf("=====Absences=====!\n");
+        printf("21) Faire l'appel des étudiants dans une matière\n");
+        printf("22) Modifier une abscence\n");
+        printf("23) Supprimer une abscence\n");
+        printf("24) Afficher les abscences\n\n");
+
+void FaireAppel(PileAbsences *p, char *matiere)
+{
+    printf("Entrez les absents : \n");
+    Absences *newAbs = (Matieres *)malloc(sizeof(Matieres));
+    newAbs->a = (char *)malloc(strlen(matiere)*sizeof(char *));
+
+    if (p == NULL || newAbs == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+   strcpy(newAbs->matiere);
+    strcpy(newAbs->matiere);
+    newAbs->suivant = p->premier;
+    p->premier = newAbs;
+    p->taille++;
+}
+
